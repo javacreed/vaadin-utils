31689ba15dc713897733ca117bb11be6f024b18a
Albert Attard <albert@javacreed.com>
2017-11-21 22:18:25 +0100

    Added a new method

--- src/main/java/com/javacreed/api/vaadin/utils/VaadinUtils.java
+++ src/main/java/com/javacreed/api/vaadin/utils/VaadinUtils.java
@@ -5,9 +5,13 @@ import com.vaadin.ui.Notification.Type;
 
 public class VaadinUtils {
 
+    public static void commingSoon(final String caption) {
+        Notification.show(caption, "This feature is being developed and will soon be ready.", Type.TRAY_NOTIFICATION);
+    }
+
     public static void maybe(final String caption) {
         Notification.show(caption,
-                "This feature is not yet implemented because Mona is not sure whether it will be used or not.  If this is something that you really like to have, then please speak with Mona about this.",
+                "This feature is not yet implemented because we are not sure whether it will be used or not.  If this is something that you really like to have, then please speak with your supervisor about this.",
                 Type.TRAY_NOTIFICATION);
     }
 

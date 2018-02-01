5256ddc5c13b226d0433c2650e4812b11c79aca1
Albert Attard <albert@javacreed.com>
2018-02-01 22:49:39 +0100

    Added a new validation

--- src/main/java/com/javacreed/api/vaadin/utils/ValidationUtils.java
+++ src/main/java/com/javacreed/api/vaadin/utils/ValidationUtils.java
@@ -1,12 +1,18 @@
 package com.javacreed.api.vaadin.utils;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.function.Consumer;
 
 import org.apache.commons.lang3.StringUtils;
 
+import com.vaadin.server.ErrorMessage;
 import com.vaadin.server.UserError;
 import com.vaadin.shared.Registration;
+import com.vaadin.ui.AbstractComponent;
+import com.vaadin.ui.AbstractLayout;
 import com.vaadin.ui.AbstractTextField;
+import com.vaadin.ui.Component;
 
 public class ValidationUtils {
 
@@ -19,6 +25,23 @@ public class ValidationUtils {
     } catch (final RuntimeException e) {
       textField.setComponentError(new UserError("Failed to validate the input"));
     }
+  }
+
+  public static List<ErrorMessage> getChildrenErrors(final AbstractLayout layout) {
+    final List<ErrorMessage> messages = new ArrayList<>();
+
+    for (final Component component : layout) {
+      if (component instanceof AbstractComponent) {
+        final ErrorMessage errorMessage = ((AbstractComponent) component).getComponentError();
+        if (errorMessage == null) {
+          continue;
+        }
+
+        messages.add(errorMessage);
+      }
+    }
+
+    return messages;
   }
 
   public static Registration onBlur(final AbstractTextField textField, final Consumer<String> consumer) {
